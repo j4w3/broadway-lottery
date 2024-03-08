3c8eb17d97855b956eeb88a6d943249bd832f1d1
Jason <github@thingymail.com>
2024-03-08 00:59:38 -0500

    Update shows

--- e2e/broadway-direct.spec.ts
+++ e2e/broadway-direct.spec.ts
@@ -14,10 +14,10 @@ chromium.use(stealth);
 const urls = [
   "https://lottery.broadwaydirect.com/show/aladdin/",
   "https://lottery.broadwaydirect.com/show/mj-ny/",
-  // "https://lottery.broadwaydirect.com/show/shucked-ny/",
   "https://lottery.broadwaydirect.com/show/six-ny/",
   "https://lottery.broadwaydirect.com/show/sweeney-todd-ny/",
-  // "https://lottery.broadwaydirect.com/show/the-lion-king/",
+  "https://lottery.broadwaydirect.com/show/the-lion-king/",
+  "https://lottery.broadwaydirect.com/show/tommy-ny/",
   "https://lottery.broadwaydirect.com/show/wicked/",
 ];
 
